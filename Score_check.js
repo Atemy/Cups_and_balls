f882b9340caef123de0a729139c2250e1e6e0999
Sokol <silvana93@mail.ru>
2021-10-25 15:41:26 +0300

    Добавлена библиотека GSAP, Подсказка в консоли, мелкие исправления

--- Score_check.js
+++ Score_check.js
@@ -1,27 +1,36 @@
 let money = 10;
+let master_money = 20;
 let bet = 0;
 let userNumber;
 let full_cup_number;
 let fullCupImage = null;
 let doc_money = document.querySelector('.money');
+let doc_master_money = document.querySelector('.master-money');
 let doc_bet = document.querySelector('.bet');
 let bet_button = document.querySelector('.Place-a-bet-button');
 let bet_value = document.querySelector('.Place-a-bet-input');
-let player_message = document.querySelector('.player-message')
+let player_message = document.querySelector('.player-message');
+
 
 const buttonsArray = document.getElementsByClassName('cupButton');
 const imageArray = document.getElementsByClassName('image-cup');
 score();
+gsap.to("#player_graph", 2, {duration:1, height:money * 7});
+gsap.to("#master_graph", 2, {duration:2, height:master_money * 7});
+
+
 
 function mix_cup() {
     full_cup_number = getRandomInt(1, 4);
+    console.log(full_cup_number);
     player_message.textContent = "Теперь выбери наперсток";
     return full_cup_number;
 };
 
 function score() {
-    doc_money.textContent = "Деньги: " + money;
-    doc_bet.textContent = "Ставка: " + bet;
+    doc_money.textContent = "Деньги игрока: " + money;
+    doc_master_money.textContent="Деньги мастера: " + master_money;
+    doc_bet.textContent = "Ставка: " + 0;
 };
 
 function checkOneMore(event) {
@@ -29,12 +38,18 @@ function checkOneMore(event) {
     if (event.target.id.indexOf(`${full_cup_number}`) !== -1) {
         player_message.textContent = `Ты Угадал, молодец!`;
         money += +bet; 
+        master_money -= bet;
         disable_cups();
+        gsap.to("#master_graph", 2, {height:master_money * 7});
+        gsap.to("#player_graph", 2, {height:money * 7, delay: 2});
         score();
     } else {
         player_message.textContent = `Ха ха, мимо!`;
         money -= bet;
+        master_money += bet;
         disable_cups();
+        gsap.to("#player_graph", 2, {height:money * 7});
+        gsap.to("#master_graph", 2, {height:master_money * 7, delay: 2});
         score();
     }
 };
